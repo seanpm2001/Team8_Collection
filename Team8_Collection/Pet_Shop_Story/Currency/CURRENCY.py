710975a9015c37fab2d1fa6c935f2773302d2ad9
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-12-31 18:01:01 -0800

    CURRENCY.py (V1)
    Version 1 of the currency file for this directory.

---
+++ Team8_Collection/Pet_Shop_Story/Currency/CURRENCY.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Start of script
+# Currency for Pet Shop Story
+
+# Starter amounts
+coinsStart = int(20000)
+purpleGemsStart = int(100)
+
+# Current amount
+''' I don't know how to store data in Python yet, so the starter values will be employed here for now '''
+coins = int(20000)
+purpleGems = int(100)
+
+# Max amount
+coinsMax = int(170141183460469231731687303715884105728) # 128 bit integer (2^128)
+purpleGemsMax = int(18446744073709551615) # 64 bit integer (2^64)
+
+""" File info
+File type: Python 3 source file (*.py)
+File version: 1 (2021, Friday, December 31st at 6:01 pm)
+Line count (including blank lines and compiler line): 25
+"""
+
+# End of script
